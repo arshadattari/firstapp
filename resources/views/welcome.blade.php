6434b4e17b9b163f5be02e533497eea454aeeefa
arshadattari <syedtech110@gmail.com>
2015-12-14 15:32:30 +0500

    Change Prject Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">My First Laravel Application</div>
             </div>
         </div>
     </body>
